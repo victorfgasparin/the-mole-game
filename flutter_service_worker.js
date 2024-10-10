af9c80bbb70eac086ddf1f4ff4909daaac839422
Victor Ferme Gasparin <victor.maximillian@gmail.com>
2024-10-10 02:26:34 -0300

    build 3

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -282,7 +282,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "fcfd986eaca21f837b477eb55bdc714e
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "c80efbc0ec74a020ea15b813ddb7a6ab",
+"flutter_bootstrap.js": "897d8ca7542cf90490bef6a4b0446b66",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
