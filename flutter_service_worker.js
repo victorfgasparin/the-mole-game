da8246925d1c609baddf6c40ba28d90f062fbdc5
Victor Ferme Gasparin <victor.maximillian@gmail.com>
2024-10-10 13:44:44 -0300

    aaa

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -286,7 +286,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "006bcd28a72957bd091220b999541c01
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "5f68a1d5de68299831504b7399beeecc",
+"flutter_bootstrap.js": "3e5851957dd916f553e309892497e0fc",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -294,7 +294,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "006bcd28a72957bd091220b999541c01
 "icons/the-mole-icon-app-lg.png": "cfba44003d599767cf9fbbe6599156a1",
 "index.html": "3dc148dd225d301d55fd3157c489e47f",
 "/": "3dc148dd225d301d55fd3157c489e47f",
-"main.dart.js": "4bd8622c1682accd54fd00b1280acc6d",
+"main.dart.js": "574761d99240fca96feccc8df901c6d8",
 "manifest.json": "a2da4e69e60649fdfd0051006849140c",
 "the-mole-icon-app.png": "fb40e54314dcf82537e0094a3956b663",
 "version.json": "70c75f0871c00f25a7f85ae889c60c32"};
