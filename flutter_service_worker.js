047b598dfc26ea20597e6b971589b1646ccd0f48
Victor Ferme Gasparin <victor.maximillian@gmail.com>
2024-10-10 08:26:54 -0300

    asasa

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -286,7 +286,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "006bcd28a72957bd091220b999541c01
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "f90a83ae585229638970c76c69f34be0",
+"flutter_bootstrap.js": "5f68a1d5de68299831504b7399beeecc",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
